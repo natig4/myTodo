01af8f5dc42f419ed4ec277d63a42f26dea03e15
Nati Gurevich <nati_g4@hotmail.com>
2022-02-11 23:12:35 +0200

    About page added

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,6 +1,17 @@
 const AboutUs = () => {
   return (
-    <div className='about-container flex column align-center'>AboutUs!!</div>
+    <section className='about-container flex column align-center'>
+      <h1>Welcome To the About Page</h1>
+      <div className='creator flex column align-center'>
+        <h3>This page was created by Nati Gurevich</h3>
+        <div>
+          <img
+            src='https://res.cloudinary.com/dcwcv78uf/image/upload/v1644613465/NatiGurevich_wd00kj.jpg'
+            alt=''
+          />
+        </div>
+      </div>
+    </section>
   );
 };
 export default AboutUs;
